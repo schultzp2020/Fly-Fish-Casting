5b836a4a4741f117d831646c3c0e65fde3f33878
Paul Schultz <tanksrule1015@gmail.com>
2021-03-12 13:38:19 -0600

    fixed some issues

--- Flyline_Generator.py
+++ Flyline_Generator.py
@@ -11,20 +11,18 @@ def get_y(phi, i, yt, length):
     s+= length[i] * np.sin(phi[i]) * 0.5 
     return s
 
-npts = 24
+npts = 60
 
-length = 0.33 * np.ones(npts)
+length = 0.1 * np.ones(npts)
 radius = 0.0005205 * np.ones(npts)
-mass = 1158*np.pi* np.power(0.0005205,2) * 0.33 * np.ones(npts)
-phi_init = (-np.pi/3.) * np.ones(npts)
+mass = 1158*np.pi* np.power(0.0005205,2) * 0.1 * np.ones(npts)
+phi_init = (-np.pi/2) * np.ones(npts)
 phi_dot_init = np.zeros(npts)
 
 for i in range(npts):
-  y_pos = get_y(phi_init, i, 0.5, length)
+  y_pos = get_y(phi_init, i, 3, length)
   if y_pos < 0:
-    phi_init[i-1] = 0.0
-    if i + 1 == npts:
-      phi_init[i] = 0.0
+    phi_init[i] = 0.0
 
 data = {"Length": length, "Radius": radius, "Mass": mass, "Phi Init": phi_init, "Phi Dot Init": phi_dot_init}
 columns = ["Length", "Radius", "Mass", "Phi Init", "Phi Dot Init"]
